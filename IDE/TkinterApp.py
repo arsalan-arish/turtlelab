d0f60f94384d81df0d7adb70a72aefcd72e60ea2
Arsalan Arish <mail@arsalanarish.com>
2026-04-22 16:16:25 +0500

    Made progress

--- IDE/TkinterApp.py
+++ IDE/TkinterApp.py
@@ -12,7 +12,7 @@ class TkinterApp(ABC):
         """Set initial config, and call appropriate methods"""
 
     @abstractmethod
-    def bind_shortcuts(self):
+    def bind_events(self):
         """Bind all the shortcuts to their respective functions"""
 
     @abstractmethod
@@ -54,28 +54,101 @@ class TkinterApp(ABC):
         for name, func in optionsAndFunctions.items():
             sub_menu.add_cascade(label=name, command=func)
 
-    def open_file(self) -> str:
+    def promptForFile(self) -> (str, Path, str):
         filepath = filedialog.askopenfilename(
             filetypes=[("All files", "*.*")],
             initialdir=os.getcwd()
         )
-        if not filepath: return
+        if not filepath: return None, None, None
         with open(filepath, "r") as f:
-            return f.read()
+            return (Path(filepath).name, Path(filepath), f.read())
+        
+    def promptForFolder(self) -> Path:
+        folderpath = filedialog.askdirectory(
+            mustexist=True,
+            initialdir=os.getcwd(),
+        )
+        if not folderpath: return None
+        return Path(folderpath)
 
-    def save_as(self, data: str):
+    def promptForSaveAsFile(self) -> (str, Path):
         filepath = filedialog.asksaveasfilename(
-            filetypes=[("All files", "*.*")],
+            filetypes=[("Text files", "*.txt"), ("All files", "*.*")],
+            defaultextension=".txt",
             initialdir=os.getcwd(),
             title="Save file as"
         )
-        if not filepath: return
-        with open(filepath, "w") as f:
-            f.write(data)
+        if not filepath: return None, None
+        return (Path(filepath).name, Path(filepath))
 
+    def popMessageBox(self, level: str, title: str, msg: str):
+        match(level):
+            case "info":
+                messagebox.showinfo(title, msg)
+            case "warning":
+                messagebox.showwarning(title, msg)
+            case "error":
+                messagebox.showerror(title, msg)
+        
     def getCenteredDimensions(self, width, height, root) -> str:
         dwidth = root.winfo_screenwidth()
         dheight = root.winfo_screenheight()
         center_x = int(dwidth/2 - width / 2) - 25
         center_y = int(dheight/2 - height / 2) - 35 
         return f"{width}x{height}+{center_x}+{center_y}"
+
+    def add_tabSpaceBlock(self, id: int, textVar: StringVar):
+        tabSpace = self.components["tabSpace"]
+        block = Frame(tabSpace, height=35, width=100, relief="solid", bd=1)
+        block.pack_propagate(False)
+        block.id = id
+        f = Frame(block); f.pack(side="left")
+        l = Label(f, textvariable=textVar, padx=10)
+        cross = Button(block, text="✖", command=lambda: self.rmTab(id), bg="white", width=1, height=1)
+        l.pack(anchor="center")
+        cross.pack(side="right", anchor="center", fill="y")
+        block.pack(side="left")
+        
+        block.bind("<ButtonPress-1>", lambda e: self.activateTab(id))
+        l.bind("<ButtonPress-1>", lambda e: self.activateTab(id))
+
+    def rm_tabSpaceBlock(self, id: int):
+        tabSpace = self.components["tabSpace"]
+        for child in tabSpace.winfo_children():
+            if child.id == id:
+                child.pack_forget()
+                child.destroy()
+                
+    def add_editorSpaceBlock(self, id: int, widget: Any):
+        editorSpace = self.components["editorSpace"]
+        assert widget in editorSpace.winfo_children()
+        widget.id = id
+
+    def rm_editorSpaceBlock(self, id: int):
+        editorSpace = self.components["editorSpace"]
+        for child in editorSpace.winfo_children():
+            if child.id == id:
+                child.destroy()
+
+class FileObject:
+    def __init__(self, id: int, nameVar: StringVar, filepath: Path, editor: Text):
+        """id is synced with the tab id, editor is the instance of the Text widget object that holds the file content"""
+        self.id = id
+        self.nameVar = nameVar
+        self.filepath = filepath
+        self.editor = editor
+
+    def save(self):
+        data = self.editor.get("1.0", "end-1c")
+        with open(self.filepath, "w") as f:
+            f.write(data)
+    
+
+class Editor(Text):
+    def __init__(self):
+        super().__init__(self.components["editorSpace"], font=("Cascadia Code", 14), wrap="word", undo=True)
+        
+
+        # editor.bind("<Return>", self._smart_indent)
+        # editor.bind("<Tab>", self._tab)
+        # editor.bind("<Control-Return>", self.run_code)
--- IDE/turtle_IDE.py
+++ IDE/turtle_IDE.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from TkinterApp import TkinterApp
+from TkinterApp import TkinterApp, FileObject, Editor
 from pathlib import Path
 import json
 import os
@@ -24,7 +24,9 @@ class TurtleLab(TkinterApp):
             "sidebarVisible": True,
             "panelVisible": True,
             "commandPanelVisible": False,
-            "tabSelected": None, # or id of the tabBlock-editorBlock pair (tab)
+            "activeTab": None,
+            "tabIdCounter": 1,
+            "FileObjects": []
         }
 
         self.bind_to_self (
@@ -34,17 +36,20 @@ class TurtleLab(TkinterApp):
         )
 
         self.build_menu()
-        self.bind_shortcuts()
+        self.bind_events()
         self.build_widget_tree()
         self.build_layout(["all"])
-        self.add_tabSpaceBlock(1, "file")
-        self.add_editorSpaceBlock(1, Text(self.components["editorSpace"]))
-        self.activateTab(1)
+        
 
 
-    def bind_shortcuts(self):
+    def bind_events(self):
+        # General shortcuts
         self.root.bind("<Control-Shift-P>", lambda e: self.toggleComponent("commandPanel"))
         self.root.bind("<Escape>", lambda e: self.handleEscape())
+        self.root.bind("<Control-n>", lambda e: self.menu_newFile())
+        self.root.bind("<Control-s>", lambda e: self.menu_save())
+        self.root.bind("<Control-Shift-S>", lambda e: self.menu_saveAs())
+
 
 
     def build_widget_tree(self):
@@ -55,12 +60,12 @@ class TurtleLab(TkinterApp):
 
         activityBar = Frame(sideframe, width=45);             activityBar.pack_propagate(False)
         sidebar = Frame(sideframe, width=280);                sidebar.pack_propagate(False)
-        tabSpace = Frame(mainframe, height=35, bg="grey");    tabSpace.pack_propagate(False)
+        tabSpace = Frame(mainframe, height=35, bg="white");    tabSpace.pack_propagate(False)
         editorSpace = Frame(mainframe);                        editorSpace.pack_propagate(False)
         panel = Frame(mainframe, height=250, bg="purple");    panel.pack_propagate(False)
 
-        Button(tabSpace, width=1, height=1, command=lambda: self.toggleComponent("panel")).pack(side="right")
-        Button(tabSpace, width=1, height=1, command=lambda: self.toggleComponent("sidebar")).pack(side="right")
+        b = Button(tabSpace, width=1, height=1, text="⬇", command=lambda: self.toggleComponent("panel")); b.pack(side="right"); b.id = None
+        b = Button(tabSpace, width=1, height=1, text="⬅", command=lambda: self.toggleComponent("sidebar")); b.pack(side="right"); b.id = None
         commandPanel.childs = {}
         commandPanel.childs["entry"] = Entry(commandPanel)
         commandPanel.childs["entry"].place(relx=0.5, y=150, anchor="center", width=520)
@@ -120,58 +125,54 @@ class TurtleLab(TkinterApp):
             self.components[component].place_forget()
             self.components[component].grid_forget()
 
-    
-    def add_tabSpaceBlock(self, id: int, text: str):
-        tabSpace = self.components["tabSpace"]
-        block = Frame(tabSpace, height=35, width=100, relief="solid", bd=1)
-        block.pack_propagate(False)
-        block.id = id
-        Label(block, text=text).pack(anchor="center")
-
-    def rm_tabSpaceBlock(self, id: int):
-        tabSpace = self.components["tabSpace"]
-        for child in tabSpace.winfo_children():
-            if child.id == id:
-                child.pack_forget()
-                child.destroy()
-                
-
-    def add_editorSpaceBlock(self, id: int, widget: Any):
-        editorSpace = self.components["editorSpace"]
-        assert widget in editorSpace.winfo_children()
-        widget.id = id
-
-    def rm_editorSpaceBlock(self, id: int):
-        editorSpace = self.components["editorSpace"]
-        for child in editorSpace.winfo_children():
-            if child.id == id:
-                child.destroy()
 
     # editorSpaceBlock + tabSpaceBlock = Tab
+    def createTab(self, id: int, nameVar: StringVar, widget: Any):
+        self.add_tabSpaceBlock(id, nameVar)
+        self.add_editorSpaceBlock(id, widget)
+
+    def rmTab(self, id: int):
+        self.deactivateTab(id)
+        self.rm_tabSpaceBlock(id)
+        self.rm_editorSpaceBlock(id)
+
     def activateTab(self, id: int):
-        if tabId := self.state["tabSelected"]:
+        if tabId := self.state["activeTab"]:
             self.deactivateTab(tabId)
-        self.state["tabSelected"] = id
-        tabSpaceBlock = [child for child in self.components["tabSpace"].winfo_children() if type(child) == Frame and child.id == id].pop()
-        # This little type check (in the end) will make sure it does not consider the other two child buttons inside tabSpace Frame
+        
+        self.state["activeTab"] = id
+        tabSpaceBlock = [child for child in self.components["tabSpace"].winfo_children() if child.id == id].pop()
         editorSpaceBlock = [child for child in self.components["editorSpace"].winfo_children() if child.id == id].pop()
 
-        tabSpaceBlock.configure(bg="grey", bd=2)
+        tabSpaceBlock.configure(bg="grey", bd=2, relief="raised")
         editorSpaceBlock.pack(fill="both", expand=True)
 
     def deactivateTab(self, id: int):
-        self.state["tabSelected"] = None
+        self.state["activeTab"] = None
         tabSpaceBlock = [child for child in self.components["tabSpace"].winfo_children() if child.id == id].pop()
         editorSpaceBlock = [child for child in self.components["editorSpace"].winfo_children() if child.id == id].pop()
 
         tabSpaceBlock.configure(bg="white", bd=1)
         editorSpaceBlock.pack_forget()
 
+
     def handleEscape(self):
         if self.state["commandPanelVisible"]:
             self.toggleComponent("commandPanel")
             self.components["commandPanel"].childs["entry"].delete(0, "end")
         
+    def getNewTabId(self) -> int:
+        id = self.state["tabIdCounter"]
+        self.state["tabIdCounter"] += 1
+        return id
+
+    def getCodeEditor(self) -> Editor:
+        editor = Editor()
+        return editor
+
+    def loadDirectory(self, path: Path):
+        os.chdir(path)
+
     #! =================== MENU UTILITIES FUNCTIONS START HERE =================== #!
 
     def build_menu(self):
@@ -183,6 +184,7 @@ class TurtleLab(TkinterApp):
             {
                 "New File": self.menu_newFile,
                 "Open File": self.menu_openFile,
+                "Open Folder": self.menu_openFolder,
                 "Save": self.menu_save,
                 "Save As": self.menu_saveAs,
             }
@@ -197,13 +199,57 @@ class TurtleLab(TkinterApp):
         )
 
     def menu_newFile(self):
-        pass
+        id = self.getNewTabId()
+        editor = self.getCodeEditor()
+        editor.focus()
+        self.createTab(id, StringVar(value="New"), editor)
+        self.activateTab(id)
+
     def menu_openFile(self):
-        pass
+        id = self.getNewTabId()
+        filename, filepath, data = self.promptForFile()
+        if not filename: return
+        filename = StringVar(value=filename)
+        editor = self.getCodeEditor()
+        editor.insert("end", data)
+        editor.focus()
+        self.createTab(id, filename, editor)
+        self.activateTab(id)
+        self.state["FileObjects"].append(FileObject(id, filename, filepath, editor))
+
+    def menu_openFolder(self):
+        path = self.promptForFolder()
+        if not path: return
+        self.loadDirectory(path)
+
     def menu_save(self):
-        pass
+        activeTabId = self.state["activeTab"]
+        if not activeTabId: self.popMessageBox("info", "Save File", "Please select an appropriate file tab to save the file"); return
+        for obj in self.state["FileObjects"]:
+            if obj.id == activeTabId:
+                obj.save()
+                return
+        # At this point, it means that the current active tab is not associated with a file object (either it is a new file or not a file at all)
+        editorSpaceWidget = [child for child in self.components["editorSpace"].winfo_children() if child.id == activeTabId].pop()
+        if type(editorSpaceWidget) == Text:
+            self.menu_saveAs()
+        else:
+            self.popMessageBox("info", "Save File", "Please select an appropriate file tab to save the file")
+
     def menu_saveAs(self):
-        pass
+        activeTabId = self.state["activeTab"]
+        if not activeTabId: self.popMessageBox("info", "Save File", "Please select an appropriate file tab to save the file"); return
+        name, path = self.promptForSaveAsFile()
+        if not name: return
+        name = StringVar(value=name)
+        editor = [child for child in self.components["editorSpace"].winfo_children() if child.id == activeTabId].pop()
+        # Change the name label of the tab block
+        tabBlock = [child for child in self.components["tabSpace"].winfo_children() if child.id == activeTabId].pop()
+        tabBlockLabel = tabBlock.winfo_children()[0].winfo_children()[0].configure(textvariable=name)
+        fileObj = FileObject(activeTabId, name, path, editor)
+        fileObj.save()
+        self.state["FileObjects"].append(fileObj)
+
 
     def menu_cut(self):
         pass
